a8d44f7d6efc778357a245def632a1eabc13525b
gabzin <anonplayers@gmail.com>
2021-11-24 11:37:10 -0300

    Error 404 handling

--- YouTubeDownloader/urls.py
+++ YouTubeDownloader/urls.py
@@ -1,11 +1,9 @@
-from django.conf.urls import handler404, handler500
 from django.urls import path
+from django.views.generic import RedirectView
 from .views import download_video
 
-handler404 = 'YouTubeDownloader.views.error_404'
-handler500 = 'YouTubeDownloader.views.error_500'
 
 urlpatterns = [
-    path('', download_video)
+    path('', download_video),
+    path('<string>', RedirectView.as_view(url='/'))    # Exceptions handling
 ]
-
