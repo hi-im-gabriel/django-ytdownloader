e649c332822a7d2734f59ffcbc8f947584f5b695
gabzin <anonplayers@gmail.com>
2021-12-22 10:14:57 -0300

    2021.12 version

--- YouTubeDownloader/views.py
+++ YouTubeDownloader/views.py
@@ -1,16 +1,15 @@
 #Imports
+from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.contrib import messages
-from django.http import FileResponse
 from .forms import DownloadForm
 from pytube import YouTube
 from math import pow, floor, log
 from datetime import timedelta
 from requests import get
-from os import system
 
 # Your YouTube V3 Api Key
-KEY = "AIzaSyD6-aEBMd2R0m_4VOqnq_Vj0866_enwjug"
+KEY = ""
 
 # Convert from bytes
 def convertsize(size_bytes):
@@ -84,7 +83,7 @@ def download_video(request, string=""):
         # Full content to render
         context = {
             'form' : form,'title' : yt_obj.title,
-            'rating': video_likes, 'rating_check' : int(rating) + 1 if float(int(rating)) != rating else 6,
+            'rating': humanformat(int(video_likes)), 'rating_check' : int(rating) + 1 if float(int(rating)) != rating else 6,
             'rating_list' : [1,2,3,4,5], 'thumb' : yt_obj.thumbnail_url, 'author' : yt_obj.author,
             'author_url' : yt_obj.channel_url,
             'duration' : str(timedelta(seconds=yt_obj.length)), 'views' : humanformat(yt_obj.views) if yt_obj.views >= 1000 else yt_obj.views,
@@ -104,9 +103,11 @@ def btn_video(request):
             extension = selected.mime_type.replace('video/','').replace('audio/','')
             file_name = yt_obj.title + '.' + extension
 
-            response = FileResponse(open(selected.download(skip_existing=True), 'rb'), filename=file_name, as_attachment=True)
+            #response = FileResponse(open(selected.download(), 'rb'), filename=file_name, as_attachment=True)
 
-            system(f'rm *.{extension}')
+            #system(f'rm *.{extension}')
+            response = HttpResponse(selected.url, content_type='application/'+selected.mime_type)
+            response['Content-Disposition'] = f'attachment; filename="{file_name}.{extension}"'
 
             return response
 
--- readme.md
+++ readme.md
@@ -4,15 +4,29 @@
       <img src="https://img.shields.io/badge/license-MIT-red.svg" />
     </a>
     <a href="https://www.python.org/">
-    	<img src="https://img.shields.io/badge/built%20with-Django-red.svg" />
+    	<img src="https://img.shields.io/badge/built%20with-Django-green.svg" />
     </a>
 	<p align="center">⭐️ Star this project ⭐️</p>
   </p>
+<a href="https://downtube.vercel.app/"><p align="center">https://downtube.vercel.app/</p></a>
 <p align="center">
-  <img src="https://i.imgur.com/UGBIj8m.png"">
+  <a href="https://downtube.vercel.app/"><img src="https://iili.io/YfwYva.png"></a>
 </p>
 
 # Updates
+> # v2021.12
+> 
+> #### Added:
+> 
+> - YouTube API to get likes/favs count
+> - Visual enhancement
+> - Particles JS
+> - Bootstrap icons
+>
+> #### Bug fixes:
+>
+> - Opening video/audio links instead of downloading
+>
 > # v2021.11
 > 
 > #### Added:
@@ -31,16 +45,24 @@
 
 
 # Requirements
-  - Python3+
+  - Python3
+  - Pip3
   - Git
+  - YouTube V3 API KEY (otherwise, likes count aren't avalible with pytube anymore):
+  
+  <a href="https://developers.google.com/youtube/v3">https://developers.google.com/youtube/v3</a>
+  
+ <p align="center">
+    <img src="https://iili.io/YfwpF1.png" width="80%">
+</p>
+  
 
 # Installation
 
 ```bash
 git clone https://github.com/gabzin/django-ytdownloader
 cd django-ytdownloader
-pip install -r requirements.txt
-python manage.py migrate
+pip3 install -r requirements.txt
 ```
 
 # Starting server
